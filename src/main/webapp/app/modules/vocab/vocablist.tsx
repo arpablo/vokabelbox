d82fd08d3ac95c49cf3e1e0b11db91d01d759d15
Armin Pfarr <armin.pfarr@gmail.com>
2018-07-20 10:40:11 +0200

    Changes
    Signed-off-by: Armin Pfarr <armin.pfarr@gmail.com>

--- src/main/webapp/app/modules/vocab/vocablist.tsx
+++ src/main/webapp/app/modules/vocab/vocablist.tsx
@@ -1,13 +1,39 @@
 import * as React from "react";
 import { Button, Table, Row, Badge } from "reactstrap";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 
-export class VocabList extends React.Component {
+export default class VocabList extends React.Component {
   render() {
     return (
       <div>
         <Table responsive striped>
-          <thead />
-          <tbody />
+          <thead>
+            <tr>
+              <th>Original</th>
+              <th>Übersetzung</th>
+              <th>Aktion</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Test</td>
+              <td>teste</td>
+              <td className="text-right">
+                <Button>
+                  <FontAwesomeIcon icon="eye" />
+                  View
+                </Button>
+                <Button>
+                  <FontAwesomeIcon icon="pencil-alt" />
+                  Edit
+                </Button>
+                <Button>
+                  <FontAwesomeIcon icon="trash" />
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          </tbody>
         </Table>
       </div>
     );
