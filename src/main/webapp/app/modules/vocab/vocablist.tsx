5e84a3af12a357ed434d54e11ba2237d9aa62e91
Armin Pfarr <armin.pfarr@gmail.com>
2018-07-20 09:01:06 +0200

    changes vocab.tsx

---
+++ src/main/webapp/app/modules/vocab/vocablist.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Button, Table, Row, Badge } from "reactstrap";
+
+export class VocabList extends React.Component {
+  render() {
+    return (
+      <div>
+        <Table responsive striped>
+          <thead />
+          <tbody />
+        </Table>
+      </div>
+    );
+  }
+}
